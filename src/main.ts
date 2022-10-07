227e1711400808927ca6c9c91bb7d886e8bc2a48
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-10-07 19:05:01 +0600

    feat:
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,26 @@
-console.log('hello world')
+import { Server } from 'socket.io'
+import express from 'express'
+import { createServer } from 'http'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+app.use(express.json())
+
+const httpServer = createServer(app)
+const io = new Server(httpServer, {
+	cors: {
+		origin: '*',
+	},
+})
+
+io.on('connection', () => {
+	console.log('client connected')
+})
+
+const PORT = process.env.PORT === '' ? 3000 : process.env.PORT
+httpServer.listen(PORT, () => {
+	console.log(`server running on port ${PORT}`)
+})
